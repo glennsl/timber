37f940305d10efb9406f854a3993f5853855ccc8
glennsl <glenn@slotte.net>
2020-01-19 08:30:59 +0100

    adapt parts of nativeChannels.c from reason-native/console

---
+++ src/nativeChannels.c
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+ // This file has been adapted from reason-native/console:
+ // https://github.com/facebookexperimental/reason-native/blob/master/src/console/nativeChannels.c
+
+#define CAML_NAME_SPACE
+
+#include <stdio.h>
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
+
+CAMLprim value native_error(value str)
+{
+  CAMLparam1(str);
+  fprintf(stderr, "%s", String_val(str));
+  CAMLreturn(Val_unit);
+}
